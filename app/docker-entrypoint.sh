db582668e480f299eb1e035d298749ff6e0a61fe
Shane <6071159+smashedr@users.noreply.github.com>
2023-07-05 18:37:18 -0700

    deploy

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env sh
 
+set -ex
+
 if echo "${*}" | grep -q "gun";then
     if [ -n "${DJANGO_SUPERUSER_PASSWORD}" ] &&
     [ -n "${DJANGO_SUPERUSER_USERNAME}" ] &&
     [ -n "${DJANGO_SUPERUSER_EMAIL}" ];then
-        python manage.py createsuperuser --noinput
+        python manage.py createsuperuser --noinput || :
     fi
-    set -ex
     python manage.py collectstatic --noinput
 fi
-
-set -ex
 
 exec "$@"
